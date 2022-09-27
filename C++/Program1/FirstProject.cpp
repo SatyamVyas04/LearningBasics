2ddf77cd1982c19e33ce5ef294e84dd8b96739a6
SatyamVyas <vyas.satyam.04@gmail.com>
2022-09-27 16:08:09 +0530

    Initial Commit

---
+++ C++/Program1/FirstProject.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Satyam"<<endl;
+    return 0;
+
+}
---
+++ Python/SchoolRelated/Term2File/Animation.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+string = "Redirecting in..."
+string2 = "Redirecting..."
+for letter in string: 
+    print(letter, end= "")
+    sleep(0.05)
+
+for i in range():
+    print(i, end=" ")
+    sleep(1)
+print()
+
+for i in string2:
+    print(i)
+    sleep(0.05)
