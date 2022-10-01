14436185876645726f14dbc8b8fea5ad75badeca
SatyamVyas <vyas.satyam.04@gmail.com>
2022-10-01 12:46:55 +0530

    New Method Details added

--- Python/Classes.py
+++ Python/Classes.py
@@ -6,14 +6,23 @@ class Employee:
         self.salary = salary
         self.email = fname + "." + lname + "@Python.com"
 
-    
     def fullname(self):
         return "{} {}".format(self.first, self.last)
-
+    
+    def Details(self):
+        print(f" Details of {self.fullname()} are as follows ".center(70, "-"))
+        print("|"+"EmployeeName".center(20, " "), end="|")
+        print("EmployeeEmail".center(30, " "), end="|")
+        print("EmployeeSalary".center(16, " "), end="|\n")        
+        print("-".center(70, "-"))
+        print("|"+f"{self.fullname()}".center(20, " "), end="|")
+        print(f"{self.email}".center(30, " "), end="|")
+        print(f"{self.salary}".center(16, " "), end="|\n") 
+        print("-".center(70, "-"))
+        print()    
 
 emp1 = Employee("Satyam", "Vyas", int('100_000'))
 emp2 = Employee("Test", "User", int('50_000'))
 
-print(emp1.email)
-print(emp2.email)
-print(emp1.fullname())
+emp1.Details()
+emp2.Details()
