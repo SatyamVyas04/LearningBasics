53430f204924090ece4a9767e3e0d5d2c7e8ea97
SatyamVyas <vyas.satyam.04@gmail.com>
2022-10-09 23:21:24 +0530

    New Files added

---
+++ Python/Comp.Testing/test_pytests.py
@@ -0,0 +1,7 @@
+import TestFile    # The code to test
+
+def test_increment():
+    assert TestFile.increment(3) == 4
+
+def test_decrement():
+    assert TestFile.decrement(3) == 4
---
+++ Python/Test_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+# Code
+for _ in range(int(input())):
+    size = int(input())
+    s = input()
+    l = []
+    for _ in range(size):
+        t = s[0]
+        s = s[1:]
