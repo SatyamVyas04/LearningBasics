58afa2a924ff8599b3f7df9e709257983bf8c86f
SatyamVyas <vyas.satyam.04@gmail.com>
2022-10-09 23:42:59 +0530

    Changes made

--- Python/Comp.Testing/TestFile.py
+++
@@ -1,8 +0,0 @@
-# Code
-def main(a, b):
-    size = int(a)
-    s = b
-    l = []
-    for _ in range(size):
-        t = s[0]
-        s = s[1:]
--- Python/Comp.Testing/test_pytests.py
+++
@@ -1,7 +0,0 @@
-import TestFile    # The code to test
-
-def test1():
-    assert TestFile.main("2", "10") == 4
-
-def test2():
-    assert TestFile.main("4", "0001") == 4
