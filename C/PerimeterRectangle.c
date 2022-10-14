fecdda0504a28211fe1ca230e723f7c63c9b8d44
SatyamVyas <vyas.satyam.04@gmail.com>
2022-10-15 00:53:34 +0530

    Changes Made

---
+++ C/PerimeterRectangle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// Perimeter of Square
+int main() {
+    float a;
+    printf("Enter the Side of the Square: ");
+    scanf("%f", &a);
+    printf("The Perimeter of the Square is %f", 4*a);
+    return 0;
+}
