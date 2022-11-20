ead3f260ebaeb9dae3d640c49ab66092ff85669d
SatyamVyas <vyas.satyam.04@gmail.com>
2022-11-20 19:37:36 +0530

    Arrays in C learnt

---
+++ C/Arrays.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    int array[5] = {0, 0, 0, 0, 0};
+    array[0] = inp(0);
+    array[1] = inp(1);
+    array[2] = inp(2);
+    array[3] = inp(3);
+    array[4] = inp(4);
+    printf("[%d, ", array[0]);
+    printf("%d, ", array[1]);
+    printf("%d, ", array[2]);
+    printf("%d, ", array[3]);
+    printf("%d]", array[4]);
+    return 0;
+}
+
+int inp(int index){
+    int val;
+    printf("Enter %d number in the array: ", index);
+    scanf("%d", &val);
+    return val;
+
+}
