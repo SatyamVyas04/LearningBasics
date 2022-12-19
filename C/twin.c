548e4ada08e7adcdc59bd7873c65d388a57e1fd6
Satyam Vyas <vyas.satyam.04@gmail.com>
2022-12-19 14:42:38 +0530

    Added comments

--- C/twin.c
+++ C/twin.c
@@ -2,7 +2,7 @@
 
 int prime(int n){
 	int d = 3;
-	for (d; d<=n/2; d+=2){
+	for (d; d<=n/2; d+=2){ //checking its divisibility against 3, 5, 7 and so on, till n/2
 		if(n%d==0){
 			return 0;
 		}
@@ -14,15 +14,15 @@ int main(){
 	int a, b;
 	printf("Enter the Range to find twin primes: ");
 	scanf("%d, %d", &a, &b);
-	// making a odd
+	// making a odd since the twin odds are all consecutive odd nums
 	if (a%2==0){
 		a++;	
 	}else if(a == 1){
 		a+=2;	
 	}
-	// traversing among the odd numbers
+	// traversing among the odd numbers to see if they are primes
 	while (a<b-2){
-		if (prime(a) == 1 && prime(a+2) == 1){
+		if (prime(a) == 1 && prime(a+2) == 1){ // consecutive odd primes
 			printf("(%d, %d)\n", a, a+2);
 		}
 		a+=2;
