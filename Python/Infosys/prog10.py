f7fa77c23b2a07a02c4d41f1c13308cecac682f3
SatyamVyas <vyas.satyam.04@gmail.com>
2023-01-22 17:26:44 +0530

    changes

--- Python/Infosys/prog10.py
+++ Python/Infosys/prog10.py
@@ -1,6 +1,6 @@
 from collections import Counter
 # driver
-s = "Work like you do not need money love like you have never been hurt and dance like no one is watching"
+s = "Work like you do not need money love like you you never been hurt and dance like no one is watching"
 arr = s.split()
 count = {}
 for i in arr:
@@ -8,23 +8,14 @@ for i in arr:
         count[i] += 1
     else:
         count[i] = 1
-        
-keys = list(count.keys())
-values = list(count.values()) 
-maxfreq = 0 
-max_repeating_word = "" 
 
-for i in range(len(values)):
-    current_value = values[i]
-    current_word = keys[i]
-    if current_value>maxfreq:
-        maxfreq = current_value
-        max_repeating_word = current_word
-    elif current_value == maxfreq:
-        if len(current_word)>len(max_repeating_word):
-            maxfreq = current_value
-            max_repeating_word = current_word
-        
-ansfreq = maxfreq
-answord = max_repeating_word
-print(answord, ansfreq)
+values = list(count.values())
+maxvalue = max(values)
+lambaword = ""
+
+for word, freq in count.items():
+    if freq == maxvalue:
+        if len(word)>=len(lambaword):
+            lambaword=word
+    
+print(lambaword, maxvalue)
