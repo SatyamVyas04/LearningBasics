716ed141dfd7fdd071f8769d7a5cd9e89d70db11
SatyamVyas <vyas.satyam.04@gmail.com>
2023-01-23 23:08:30 +0530

    Optimized

--- C/matrix.c
+++ C/matrix.c
@@ -1,50 +1,85 @@
 #include <stdio.h>
-int rows, cols;
+#include <stdlib.h>
+#include <math.h>
 
-void matrix_print(int arr[rows][cols]){
-	for(int i = 0; i<cols; i++){
-	printf("\n");
-		for(int j = 0; j<rows; j++){
-			printf("|%2d |", arr[j][i]);
-		}
-	}
+void add(int a[][10], int b[][10], int m, int n) {
+    int i, j;
+    int c[10][10];
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    printf("Addition of matrices: \n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void subtract(int a[][10], int b[][10], int m, int n) {
+    int i, j;
+    int c[10][10];
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            c[i][j] = a[i][j] - b[i][j];
+        }
+    }
+    printf("Subtraction of matrices: \n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void multiply(int a[][10], int b[][10], int m, int n, int p, int q) {
+    int i, j, k;
+    int c[10][10];
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < q; j++) {
+            c[i][j] = 0;
+        }
+    }
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < q; j++) {
+            for (k = 0; k < n; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    printf("Multiplication of matrices: \n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < q; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void input2DArray(int rows, int cols, int arr[rows][cols]) {
+    printf("Enter the elements of the 2D array:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Enter element arr[%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
 }
 
 int main() {
-    printf("Enter Rows of the Matrix: ");
+    int rows, cols;
+    printf("Enter the number of rows: ");
     scanf("%d", &rows);
-    printf("Enter Cols of the Matrix: ");
+    printf("Enter the number of columns: ");
     scanf("%d", &cols);
-    
+
     int A[rows][cols];
+    input2DArray(rows, cols, A);
     int B[rows][cols];
-    
-    for(int i = 0; i<rows; i++){
-        for(int j=0; j<cols; j++){
-            printf("Enter [%d][%d]th Element of A: ", i+1, j+1);
-            scanf("%d", &A[i][j]);
-        }
-    }
-    for(int i = 0; i<rows; i++){
-	printf("\n");
-		for(int j = 0; j<cols; j++){
-			printf("|%2d |", A[i][j]);
-		}
-	}
-	printf("\n\n");
-    for(int i = 0; i<rows; i++){
-        for(int j=0; j<cols; j++){
-            printf("Enter [%d][%d]th Element of B: ", i+1, j+1);
-            scanf("%d", &B[i][j]);
-        }
-    }
-    for(int i = 0; i<rows; i++){
-	printf("\n");
-		for(int j = 0; j<cols; j++){
-			printf("|%2d |", B[i][j]);
-		}
-	}
-	printf("\n\n");
+    input2DArray(rows, cols, B);
     return 0;
 }
-
