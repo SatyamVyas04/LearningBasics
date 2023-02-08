5fdff3bf76f0637cab20c69d9834a6c01acb4922
Satyam Vyas <vyas.satyam.04@gmail.com>
2023-02-08 09:56:17 +0530

    Update structsports.c

--- C/structsports.c
+++ C/structsports.c
@@ -16,11 +16,11 @@ void getdata(struct Player p[], int n)
 {
     for (int i = 0; i < n; i++)
     {
+    	getchar();
         printf("Enter the Name of Player %d: ", i + 1);
-        getchar();
         fgets(p[i].Name, 50, stdin);
-        printf("Enter the address of Player %d: ", i + 1);
         getchar();
+        printf("Enter the address of Player %d: ", i + 1);
         fgets(p[i].add, 100, stdin);
         printf("Enter the age of Player %d: ", i + 1);
         scanf("%d", &p[i].age);
@@ -28,40 +28,36 @@ void getdata(struct Player p[], int n)
         scanf("%d", &p[i].active);
         printf("Enter 1 if the Player %d is married or enter 0 : ", i + 1);
         scanf("%d", &p[i].married);
+        
         if (p[i].married == 1)
-        {
-            printf("Enter the Name of wife of Player %d: ", i + 1);
+        {	
             getchar();
+            printf("Enter the Name of wife of Player %d: ", i + 1);
             fgets(p[i].wife, 50, stdin);
             printf("Enter the number of children %d has: ", i + 1);
             scanf("%d", &p[i].children);
+            getchar();
             if (p[i].children == 1)
             {
                 printf("Name of child 1 of Player %d: ", i + 1);
-                getchar();
                 fgets(p[i].c1, 50, stdin);
             }
             if (p[i].children == 2)
             {
                 printf("Name of child 1 of Player %d: ", i + 1);
-                getchar();
                 fgets(p[i].c1, 50, stdin);
                 printf("Name of child 2 of Player %d:", i + 1);
-                getchar();
                 fgets(p[i].c2, 50, stdin);
             }
             if (p[i].children == 3)
             {
                 printf("Name of child 1 of Player %d: ", i + 1);
-                getchar();
                 fgets(p[i].c1, 50, stdin);
                 printf(" Name of child 2 of Player %d: ", i + 1);
-                getchar();
                 fgets(p[i].c2, 50, stdin);
                 printf(" Name of child 3 of Player %d: ", i + 1);
-                getchar();
                 fgets(p[i].c3, 50, stdin);
-                getchar();
+
             }
         }
     }
