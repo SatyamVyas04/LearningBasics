5ed659e277f7bee7eaebd7d233a353a03aa94793
SatyamVyas <vyas.satyam.04@gmail.com>
2023-02-19 19:22:29 +0530

    Practise File created

---
+++ C/LABESE1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Write a program which inputs N followed by the prices of N items. The program should
+// output the price at which each item is offered in a sale. The sale price is calculated as follows.
+// The original price is reduced by 10%, the resulting quantity raised to the nearest pound and then
+// 1 penny subtracted from it. If the resulting quantity is less than the original price then the new
+// price is output, otherwise the old price is output together with a warning message.
+
+int main(){
+
+
+}
