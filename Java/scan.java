515910da637a51d037844bc17a7efb9abd930c1e
SatyamVyas <vyas.satyam.04@gmail.com>
2023-04-08 00:57:50 +0530

    try added

--- Java/scan.java
+++ Java/scan.java
@@ -2,21 +2,22 @@ import java.util.*;
 
 public class scan {
     public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        System.out.println(a);
+        try (Scanner scanner = new Scanner(System.in)) {
+            int a = scanner.nextInt();
+            System.out.println(a);
 
-        // buffer removal
-        scanner.nextLine();
+            // buffer removal
+            scanner.nextLine();
 
-        String str = scanner.nextLine();
-        System.out.println(str);
+            String str = scanner.nextLine();
+            System.out.println(str);
 
-        String[] words = str.split(" ");
-        System.out.println(words);
+            String[] words = str.split(" ");
+            System.out.println(words);
 
-        for (String ret : words) {
-            System.out.println(ret);
+            for (String ret : words) {
+                System.out.println(ret);
+            }
         }
 
     }
