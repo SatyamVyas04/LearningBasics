f0d1af8f15fb40acca845da8a35fc771fb9073c1
SatyamVyas <vyas.satyam.04@gmail.com>
2023-04-08 12:32:09 +0530

    array and string methods done

---
+++ Java/array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class array {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> food = new ArrayList<String>();
+
+        food.add(sc.nextLine());
+        food.add(sc.nextLine());
+        food.add(sc.nextLine());
+        
+        System.out.print(food);
+    }
+}
