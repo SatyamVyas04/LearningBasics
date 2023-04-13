a4bcd8aeb19e07e94a5671f81aecec735791a41a
SatyamVyas <vyas.satyam.04@gmail.com>
2023-04-13 22:42:45 +0530

    basic method done

--- Java/primes.java
+++ Java/primes.java
@@ -1,3 +1,34 @@
+// import java.util.*;
+
+// public class primes {
+//     static int prime(int n){
+//         if(n==1) return 0;
+//         for(int i = 2; i<=Math.sqrt(n); i++){
+//             if (n%i==0){
+//                 return 0;
+//             }
+//         }
+//         return 1;
+//     }
+
+//     public static void main(String[] args){
+//         try (Scanner sc = new Scanner(System.in)) {
+//             System.out.print("Enter A: ");
+//             int a = sc.nextInt();
+//             System.out.print("Enter B: ");
+//             int b = sc.nextInt();
+//             System.out.println("Primes: ");
+//             for(int i = a; i<=b; i++){
+//                 if (prime(i)!=0){
+//                     System.out.println(i);
+//                 }
+//             }
+//         }catch(ArithmeticException e){
+//             System.out.println("Arithmetic Error! ");
+//         }
+//     }
+// }
+
 import java.util.*;
 class prime{
 	private int p;
