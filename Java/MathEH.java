781e526c7148803254cca74ef91c3e9632a87ef4
SatyamVyas <vyas.satyam.04@gmail.com>
2023-06-07 22:28:57 +0530

    Complete 9a

--- Java/MathEH.java
+++ Java/MathEH.java
@@ -1,60 +1,81 @@
 import java.util.*;
 
-class ExpressionErrors extends Exception{
-    public ExpressionErrors(String Errormsg){
+class ExpressionErrors extends Exception {
+    public ExpressionErrors(String Errormsg) {
         super(Errormsg);
     }
 }
 
-public class MathEH{
-    public static void main (String[] args){
-        String x;
+public class MathEH {
+    public static void main(String[] args) {
+        String a, b, o;
+        int num1, num2;
+        String expression;
         String[] arr = new String[3];
-        int checkFlag = 0;
-        double a, b;
-        String operator;
+        double Answer;
         Scanner sc = new Scanner(System.in);
-        
-        while(true){
-            System.out.println("\nEnter your Expression: ");
-            System.out.println("Format: <a><space><operator><space><b>");
-            System.out.print(">>> ");
-            x = sc.nextLine();
-            arr = x.split(" ");
-            
-            checkFlag = checkA(arr[0], arr[2]);
-            if(checkFlag == 0){
-                continue;
+
+        System.out.println("\n---------- Welcome to MathEH Calculator ----------");
+        while (true) {
+            System.out.println("\nFormat: <a><space><operator><space><b>");
+            System.out.print("Enter your Expression: ");
+            expression = sc.nextLine();
+            arr = expression.split(" ");
+
+            try {
+                a = arr[0].strip();
+                o = arr[1].strip().toLowerCase();
+                b = arr[2].strip();
+            } catch (ArrayIndexOutOfBoundsException A) {
+                System.out.println("> Error Occured: " + A.getMessage());
+                System.out.println("> Invalid Input Format");
+                break;
             }
-            System.out.println("> A and B Entered Correctly! ");
-            
-            checkFlag = checkOpp(arr[1]);
-            if(checkFlag == 0){
-                continue;
+
+            try {
+                numCheck(a);
+                num1 = Integer.parseInt(a);
+                numCheck(b);
+                num2 = Integer.parseInt(b);
+                Answer = operatorCheck(num1, o, num2);
+                System.out.println(">>> Answer = " + Answer);
+            } catch (ExpressionErrors E) {
+                System.out.println("> Error Occured: " + E);
             }
-            System.out.println("> Operator Entered Correctly! ");
-            System.out.println("> Order appears to be Correct! ");
+            break;
+        }
+        System.out.println("\n---------------- Thank You ----------------");
+    }
+
+    static void numCheck(String x) throws ExpressionErrors {
+        try {
+            int check = Integer.parseInt(x);
+        } catch (NumberFormatException N) {
+            throw new ExpressionErrors("\n> A and B are non Integral Values");
         }
     }
-    public static int checkA(String a, String b){
-        try{
-            int inta = Integer.parseInt(a);
-            int intb = Integer.parseInt(b);
-            return 1;
+
+    static double operatorCheck(int a, String o, int b) throws ExpressionErrors {
+        try {
+            switch (o) {
+                case "+":
+                    return a + b;
+                case "-":
+                    return a - b;
+                case "/":
+                    return a / b;
+                case "*":
+                    return a * b;
+                case "log":
+                    System.out.println(">>> Assuming A as the Base, ");
+                    return Math.log(b) / Math.log(a);
+                case "^":
+                    return Math.pow(a, b);
+                default:
+                    throw new ExpressionErrors("\n> Invalid Operator");
+            }
+        } catch (ArithmeticException A) {
+            throw new ExpressionErrors("\n> Zero Division Error");
         }
-        catch (NumberFormatException n){
-            System.out.println(n);
-            System.out.println("> A or B are not Numbers!");
-            return 0;
-        }
-    }
-    public static int checkOpp(String opp){
-        if(opp.equals("+")){
-            return 1;
-        }else{
-            System.out.println("> Invalid Operator Entered!");
-            return 0;
-        }
-        
     }
 }
