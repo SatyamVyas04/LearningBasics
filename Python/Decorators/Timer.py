aa62c6c20a52e92941df8df9efe525195218116c
SatyamVyas <vyas.satyam.04@gmail.com>
2023-07-29 13:21:26 +0530

    Decorators done

---
+++ Python/Decorators/Timer.py
@@ -0,0 +1,22 @@
+import time
+def Timer(func):
+    def wrapper(*args):
+        start = time.perf_counter()
+        x = func(*args)
+        end = time.perf_counter()
+        print(f"{func.__name__} took {end-start} ns :: Args={args}")
+    return wrapper
+
+@Timer
+def add(a):
+    sum=0
+    for i in range(1, a+1):
+        sum+=i
+    return sum
+
+@Timer
+def add2(a):
+    return int(a*(a+1)/2)
+
+add(10000000)
+add2(10000000)
