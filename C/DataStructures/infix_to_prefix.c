b6815292c750114833055efbbdafcb36ce769c93
SatyamVyas <vyas.satyam.04@gmail.com>
2023-09-04 20:06:09 +0530

    Completed Exp1

--- C/DataStructures/infix_to_prefix.c
+++ C/DataStructures/infix_to_prefix.c
@@ -19,31 +19,89 @@
 // +, - -> 3
 // others -> 4
 
-struct Stack* infix_to_prefix(char* expression) {
-  struct Stack* stack = initialize_stack(100);
-  struct Stack* prefix = initialize_stack(100);
+char *infixToPrefix(char *infix)
+{
+	int length = strlen(infix);
+	char *prefix = (char *)malloc((length + 1) * sizeof(char)); // +1 for the null terminator
+	int i, j = 0;
 
-  for (int i = sizeof(expression) / sizeof(char) - 1; i >= 0; i--) {
-    char current = expression[i];
-    if (is_operator(current) == 1 || current == ")") {
-      push(stack, current);
-    } else if (65 <= current <= 90) {
-      push(prefix, current);
-    } else {
-      char popped;
-      popped = pop(stack);
-      pop(stack);  // Extra bracket removed
-      push(prefix, popped);
-    }
-  }
-  return prefix;
+	// Reverse the infix expression
+	char *reversedInfix = (char *)malloc((length + 1) * sizeof(char));
+	for (i = length - 1; i >= 0; i--)
+	{
+		if (infix[i] == '(')
+			reversedInfix[j++] = ')';
+		else if (infix[i] == ')')
+			reversedInfix[j++] = '(';
+		else
+			reversedInfix[j++] = infix[i];
+	}
+	reversedInfix[j] = '\0';
+
+	struct Stack *stack = initialize_stack(length);
+	for (i = 0, j = 0; i < length; i++)
+	{
+		if (is_operator(reversedInfix[i]))
+		{
+			while (!isEmpty(stack) && is_higher_or_equal(reversedInfix[i], stack->array[stack->top]))
+			{
+				prefix[j++] = stack->array[stack->top];
+				stack->top--;
+			}
+			stack->top++;
+			stack->array[stack->top] = reversedInfix[i];
+		}
+		else if (reversedInfix[i] == '(')
+		{
+			stack->top++;
+			stack->array[stack->top] = reversedInfix[i];
+		}
+		else if (reversedInfix[i] == ')')
+		{
+			while (!isEmpty(stack) && stack->array[stack->top] != '(')
+			{
+				prefix[j++] = stack->array[stack->top];
+				stack->top--;
+			}
+			if (!isEmpty(stack) && stack->array[stack->top] == '(')
+				stack->top--;
+		}
+		else
+		{
+			prefix[j++] = reversedInfix[i];
+		}
+	}
+
+	// Pop any remaining operators from the stack
+	while (!isEmpty(stack))
+	{
+		prefix[j++] = stack->array[stack->top];
+		stack->top--;
+	}
+
+	prefix[j] = '\0';
+	free(reversedInfix);
+	free(stack->array);
+	free(stack);
+
+	// Reverse the prefix expression to get the final result
+	length = strlen(prefix);
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+		char temp = prefix[i];
+		prefix[i] = prefix[j];
+		prefix[j] = temp;
+	}
+	return prefix;
 }
 
-void main() {
-  char str[100];
-  printf("> Enter the Infix Expression: ");
-  scanf("%[^\n]s", str);
-  printf("%s", str);
-  struct Stack* prefix = infix_to_prefix(&exp);
-  display(prefix);
+int main()
+{
+	char infix[100];
+	printf("Enter the Infix Expression: ");
+	scanf("%[^\n]", infix);
+	char *prefix = infixToPrefix(infix);
+	printf("Prefix Expression: %s\n", prefix);
+	free(prefix);
+	return 0;
 }
--- C/DataStructures/stack.c
+++ C/DataStructures/stack.c
@@ -18,59 +18,84 @@
 // 5. isFull
 // 6. display
 
-struct Stack {
-  int top;
-  unsigned size;
-  char* array;
+struct Stack
+{
+	int top;
+	unsigned size;
+	char *array;
 };
 
 // 0 -> display
-void display(struct Stack* stack) {
-  printf("> %d <--- Top of the Stack\n", stack->array[stack->top]);
-  for (int i = 0; i < stack->size; i++) {
-    printf("> %d\n", stack->array[i]);
-  }
+void display(struct Stack *stack)
+{
+	printf("> %c <--- Top of the Stack\n", stack->array[stack->top]);
+	for (int i = stack->top - 1; i >= 0; i--)
+	{
+		printf("> %c\n", stack->array[i]);
+	}
 }
 
 // 1 -> Initialize
-struct Stack* initialize_stack(unsigned size) {
-  struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
-  stack->size = size;
-  stack->top = -1;
-  stack->array = (char*)malloc(stack->size * sizeof(char));
-  return stack;
+struct Stack *initialize_stack(unsigned size)
+{
+	struct Stack *stack = (struct Stack *)malloc(sizeof(struct Stack));
+	stack->size = size;
+	stack->top = -1;
+	stack->array = (char *)malloc(stack->size * sizeof(char));
+	return stack;
 }
 
 // 2 -> isFull
-int isEmpty(struct Stack* stack) { return stack->top == -1; }
+int isEmpty(struct Stack *stack) { return stack->top == -1; }
 
 // 3 -> isEmpty
-int isFull(struct Stack* stack) { return stack->top == stack->size - 1; }
+int isFull(struct Stack *stack) { return stack->top == stack->size - 1; }
 
 // 4 -> push
-void push(struct Stack* stack, char item) {
-  if (isFull(stack)) {
-    return;
-  }
-  stack->top++;
-  stack->array[stack->top] = item;
+void push(struct Stack *stack, char item)
+{
+	if (isFull(stack))
+	{
+		return;
+	}
+	stack->top++;
+	stack->array[stack->top] = item;
 }
 
 // 5 -> peek
-void peek(struct Stack* stack) {
-  if (isEmpty(stack)) {
-    return;
-  }
-  printf("> %d <--- Top of the Stack\n", stack->array[stack->top]);
+char peek(struct Stack *stack)
+{
+	if (isEmpty(stack))
+	{
+		return 'x';
+	}
+	printf("> %c <--- Top of the Stack\n", stack->array[stack->top]);
+	return stack->array[stack->top];
 }
 
 // 6 -> pop
-char pop(struct Stack* stack) {
-  if (isEmpty(stack)) {
-    return 0;
-  }
-  char item;
-  item = stack->array[stack->top];
-  stack->top--;
-  return item;
+char pop(struct Stack *stack)
+{
+	if (isEmpty(stack))
+	{
+		return 0;
+	}
+	char item;
+	item = stack->array[stack->top];
+	stack->top--;
+	return item;
 }
+
+// void main()
+// {
+// 	struct Stack *s = initialize_stack(10);
+// 	push(s, 'a');
+// 	push(s, 'b');
+// 	push(s, 'c');
+// 	push(s, 'd');
+// 	push(s, 'e');
+// 	peek(s);
+// 	display(s);
+// 	pop(s);
+// 	display(s);
+// }
