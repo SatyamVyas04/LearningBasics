c370ebbf086c9537c6f242d43c46c51e52e252c4
Satyam Vyas <vyas.satyam.04@gmail.com>
2023-09-18 10:42:43 +0530

    Initial

---
+++ C/DataStructures/LinkedList/add_two_numbers.c
@@ -0,0 +1,22 @@
+/*
+You are given two non-empty linked lists representing two non-negative integers. 
+The most significant digit comes first and each of their nodes contains a single digit. 
+Add the two numbers and return the sum as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+operand_1: 7 -> 2 -> 3 -> 3
+operand_2: 5 -> 7 -> 4
+
+result: 7 -> 8 -> 0 -> 7
+
+*/
+
+#include "linked_list.c"
+
+
+Node* add_two_numbers(Node* l1, Node* l2){
+
+}
+
