34b0af23e2e89a50a6717c445825e1c746963182
SatyamVyas <vyas.satyam.04@gmail.com>
2023-09-23 23:13:35 +0530

    Complete Stack and Queue in Python

--- DSA/Stack/Stack.py
+++ DSA/Stack/Stack.py
@@ -1,47 +1,45 @@
-# Prefix, Infix, Postfix and their Evaluation
-
 class Stack:
     def __init__(self):
         self.array = []
+
     def push(self, item):
         self.array.append(item)
+
     def pop(self):
-        if self.array:
-            return self.array.pop()
-        else:
-            print("> Stack is Empty!")
+        if self.is_empty():
+            return None 
+        return self.array.pop()
+
     def peek(self):
-        if Stack.isEmpty(self):
-            print("> Stack Empty!")
-            return 
-        print(f"> Top of the Stack: {self.array[-1]}")
-    def isEmpty(self):
-        if stack.array == []:
-            return True
-        return False
+        if self.is_empty():
+            return None
+        return self.array[-1]
+
+    def is_empty(self):
+        return len(self.array) == 0
+
+    def size(self):
+        return len(self.array)
+
     def display(self):
-        if Stack.isEmpty(self):
+        if self.is_empty():
             print("> Stack Empty!")
-            return 
-        print("Stack: ")
-        print("> Top of the Stack ")
-        for i in range(len(self.array)-1, -1, -1):
-            print(f"> {self.array[i]}")
+            return
+        print("Stack:")
+        print("> Top of the Stack")
+        for item in reversed(self.array):
+            print(f"> {item}")
         print("> Bottom of the Stack")
+
     def search(self, item):
-        if Stack.isEmpty(self):
+        if self.is_empty():
             print("> Stack is Empty!")
-            return 
+            return
         if item in self.array:
             print(f"> {item} found!")
-            print(f"> @ Position {self.array.index(item) + 1} from Bottom\n")
+            print(f"> @ Position {len(self.array) - self.array.index(item)} from Top\n")
         else:
             print(f"> {item} not found!")
+
     def clear(self):
         self.array = []
-        
-
-stack = Stack()
-
-stack.pop()
-stack.display()
