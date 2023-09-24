b11abec0a0b568063b314b896c5ac5fae901421b
SatyamVyas <vyas.satyam.04@gmail.com>
2023-09-24 11:45:37 +0530

    Counter completed

---
+++ JSBasics/Counter/counter.js
@@ -0,0 +1,29 @@
+let count = 0;
+countText = document.getElementById("count");
+enteries = document.getElementById("entries")
+button = document.getElementById("incr");
+rbutton = document.getElementById("reset");
+sbutton = document.getElementById("save");
+
+function increment() {
+    count = count + 1;
+    countText.innerText = count;
+}
+
+function re() {
+    count = 0;
+    countText.innerText = 0;
+}
+
+function save() {
+    if (entries.innerText === 'None') {
+        entries.innerText = count + " ";
+    }
+    else {
+        enteries.innerText += " - "  + count
+    }
+}
+
+button.addEventListener("click", increment);
+rbutton.addEventListener("click", re);
+sbutton.addEventListener("click", save)
