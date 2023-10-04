b42e1420b034bd13de03eeaed3b826948bda2b08
SatyamVyas <vyas.satyam.04@gmail.com>
2023-10-04 21:50:51 +0530

    LinkedList Basics Done

--- DSA/LinkedList/linkedlist.py
+++ DSA/LinkedList/linkedlist.py
@@ -2,47 +2,90 @@ class Node:
     def __init__(self, data: int):
         self.data = data
         self.next: Node | None = None
-    
-    
+
+
 class LinkedList:
     def __init__(self):
         self.head: Node | None = None
-        self.tail: Node | None = None
-        
+
     def __iter__(self):
         node = self.head
         while node:
             yield node.data
             node = node.next
-            
+
     def __len__(self):
         return sum(1 for _ in self)
-    
+
+    def __repr__(self):
+        return "->".join(str(item) for item in iter(self))
+
     def insert_node(self, index: int, data: int):
         new_node = Node(data)
+        if len(self) == 0:
+            self.head = new_node
+            return
         if index == 0:
-            new_node.next = self
-        elif index == -1:
-            traverse = self
-            while traverse.next != None:
+            new_node.next = self.head
+        elif index == -1 or index == len(self) - 1:
+            traverse = self.head
+            while traverse != None and traverse.next != None:
                 traverse = traverse.next
             traverse.next = new_node
-        elif index<-1 and index>len(self):
+        elif index < -1 and index > len(self):
             print("IndexError!")
         else:
-            traverse = self
-            while index>1:
+            traverse = self.head
+            while index > 1:
                 traverse = traverse.next
+                index -= 1
             new_node.next = traverse.next
             traverse.next = new_node
-        
-    def delete_node(self, index:int):
-        if index == 0:
-            self = self.next
-        if index == -1:
-            traverse = self
-            while traverse.next.next != None:
-                
-            
 
-    
+    def delete_node(self, index: int):
+        if index == 0:
+            delete_node = self.head
+            self.head = self.head.next
+            return delete_node.data
+        if index == -1 or index == len(self) - 1:
+            print('>>>>>>>')
+            delete_node = None
+            traverse = self.head
+            while traverse.next.next != None:
+                traverse = traverse.next
+            delete_node = traverse.next
+            traverse.next = None
+            return delete_node.data
+        if index > 0 and index < len(self) - 1:
+            temp = self.head
+            prev = None
+            current_index = 0
+            while temp is not None and current_index < index:
+                prev = temp
+                temp = temp.next
+                current_index += 1
+            delete_node = temp
+            prev.next = temp.next
+            return delete_node.data
+        else:
+            print("IndexError!")
+            return None
+
+
+ll = LinkedList()
+for i in range(10):
+    ll.insert_node(i, i + 1)
+
+print(len(ll))
+print(repr(ll))
+
+print(ll.delete_node(0))
+print(repr(ll))
+
+print(ll.delete_node(-1))
+print(repr(ll))
+
+print(ll.delete_node(6))
+print(repr(ll))
+
+# Basic Implementation Done
