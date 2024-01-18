8b936434a0dd7f55431d066bf128bd8a18e2326e
SatyamVyas <vyas.satyam.04@gmail.com>
2024-01-18 19:58:04 +0530

    Ready for Next Contest

--- CodingContests/CodeForces/1.py
+++
@@ -1,9 +0,0 @@
-for _ in range(int(input())):
-    for i in range(3):
-        s = set(input())
-        if "?" in s:
-            s = {"A", "B", "C"}.difference(s) 
-            ans = s
-        else:
-            pass
-    print(list(ans)[0])
--- CodingContests/CodeForces/2.py
+++
@@ -1,8 +0,0 @@
-from math import sqrt
-for _ in range(int(input())):
-    n = int(input())
-    suml = sum(list(map(int, input().split())))
-    if sqrt(suml)%1 == 0:
-        print("YES")
-    else:
-        print("NO")
