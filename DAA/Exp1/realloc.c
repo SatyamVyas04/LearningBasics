3daee337fc96e77a70c4e29fcd4340033ad07927
Satyam Vyas <vyas.satyam.04@gmail.com>
2024-01-29 10:41:12 +0530

    Create realloc.c

---
+++ DAA/Exp1/realloc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Allocate memory for 10 integers
+    int *ptr = (int *) malloc(10 * sizeof(int));
+    ptr[0] = 1;
+    ptr[1] = 2;
+    ptr[2] = 3;
+    ptr[3] = 4;
+    ptr[4] = 5;
+    ptr[5] = 6;
+    ptr[6] = 7;
+    ptr[7] = 8;
+    ptr[8] = 9;
+    ptr[9] = 10;
+
+    // Reallocate memory for 15 integers
+    ptr = (int *) realloc(ptr, 15 * sizeof(int));
+    
+    for (int i = 9; i < 15; i++) {
+        ptr[i] = i + 1;
+    }
+    for (int i = 0; i < 15; i++)
+        printf("%d ", ptr[i]);
+    free(ptr);
+    return 0;
+}
