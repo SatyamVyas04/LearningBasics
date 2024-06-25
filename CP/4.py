4060ac50b733a897070b4bcdea8ae1093e45c31f
SatyamVyas <vyas.satyam.04@gmail.com>
2024-06-25 22:37:22 +0530

    Solving 954 D

--- CP/4.py
+++ CP/4.py
@@ -1,9 +1,49 @@
-def fn(n, arr):
-    ans = 0
+def fn(n, s):
+    def sol(ps, pe, pair):
+        ans = []
+        for i in range(ps):
+            ans.append(s[i])
+            if s[i] == "1":
+                ans.append("*")
+            else:
+                ans.append("+")
+        
+        ans.append(s[ps:pe])
+        
+        for i in range(pe, n):
+            if s[i] == "1":
+                ans.append("*")
+            else:
+                ans.append("+")
+            ans.append(s[i])
+            
+        expression = "".join(ans)
+        return int(eval(expression))
     
-    return ans
+    if "0" in s:
+        return 0
+    
+    curr_sol = float("inf")
+    
+    for r in range(2, n+1):
+        l = r - 2
+        pair = int(s[l:r])
+        curr_sol = min(curr_sol, sol(l, r, pair))
+
+    return curr_sol
+    
 
 for _ in range(int(input())):
-    n = int(input)
-    arr = list(map(int, input().split()))
-    print(fn(arr))
+    n = int(input())
+    s = input()
+    if len(s) == 2:
+        print(int(s))
+    elif len(s) == 3:
+        pair1 = int(s[:1])
+        pair2 = int(s[1:])
+        if pair1 < pair2:
+            print(min(pair1 * int(s[2]), pair1 + int(s[2])))
+        else:
+            print(min(pair2 * int(s[0]), pair2 + int(s[0])))
+    else:
+        print(fn(n, s))
