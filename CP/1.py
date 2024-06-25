9bd686c7b1c658dc0626c85c6bb1ccc96c5098d4
SatyamVyas <vyas.satyam.04@gmail.com>
2024-06-25 22:00:01 +0530

    Tried solving A B C

--- CP/1.py
+++ CP/1.py
@@ -1,9 +1,17 @@
-def fn(n, arr):
-    ans = 0
-    
-    return ans
+def fn(a, b, c, d):
+    if a == c and b == d:
+        if a == b:
+            return "NO"
+        else:
+            return "YES"
+    sets = sorted([(a, c), (b, d)])
+    if sets[1][0] <= sets[0][1]:
+        if sets[1][1] > sets[0][1]:
+            return "YES"
+        return "NO"
+    return "YES"
 
 for _ in range(int(input())):
-    n = int(input)
-    arr = list(map(int, input().split()))
-    print(fn(arr))
+    a, b = map(int, input().split())
+    c, d = map(int, input().split())
+    print(fn(a, b, c, d))
--- CP/3.py
+++ CP/3.py
@@ -1,9 +1,27 @@
-def fn(n, arr):
+def fn(arr, cards):
+    n, l, r = arr
     ans = 0
-    
+    curr = 0
+    left = 0
+    for right in range(n):
+        if cards[right] in range(l, r + 1):
+            left = right
+            curr = 0
+            ans += 1
+        else:
+            curr += cards[right]
+            if curr in range(l, r + 1):
+                ans += 1
+                left = right
+                curr = 0
+            else:
+                while curr > r:
+                    curr -= cards[left]
+                    left += 1
     return ans
 
+
 for _ in range(int(input())):
-    n = int(input)
     arr = list(map(int, input().split()))
-    print(fn(arr))
+    cards = list(map(int, input().split()))
+    print(str(fn(arr, cards)))
