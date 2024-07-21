2fc20003dc6d84776a4ab83e6a09009782461aba
SatyamVyas <vyas.satyam.04@gmail.com>
2024-07-21 12:55:41 +0530

    Completed contest 960

--- CP/Template.py
+++ CP/Template.py
@@ -4,11 +4,13 @@ def int_inp():
     """
     return int(input())
 
+
 def int_list_inp():
     """
     Reads a list of integers from a single line of input.
     """
     return list(map(int, input().split()))
+
 
 def matrix_inp(n):
     """
@@ -19,6 +21,7 @@ def matrix_inp(n):
         matrix.append(list(map(int, input().split())))
     return matrix
 
+
 def matrix_inp_nbsp(n):
     """
     Reads a matrix of size n x m from input without spaces between elements.
@@ -28,23 +31,27 @@ def matrix_inp_nbsp(n):
         matrix.append(list(map(int, list(input().strip()))))
     return matrix
 
+
 def print_many(*args):
     """
     Prints multiple arguments separated by spaces.
     """
     print(" ".join(map(str, args)))
-    
+
+
 def space_join(arr):
     """
     Joins a list of integers into a single string separated by spaces.
     """
     return " ".join(map(str, arr))
 
+
 def solve():
     """
     The main logic for solving a given problem.
     """
     pass
+
 
 def main():
     """
@@ -53,6 +60,7 @@ def main():
     t = int(input())
     for _ in range(t):
         solve()
+
 
 if __name__ == "__main__":
     main()
